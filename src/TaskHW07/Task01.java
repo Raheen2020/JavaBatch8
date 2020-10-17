509c471ff822aa95919a3d5c0754337091386311
Raheen2020 <moneer.raheen@gmail.com>
2020-10-17 12:49:03 -0500

    first commit

---
+++ src/TaskHW07/Task01.java
@@ -0,0 +1,56 @@
+package TaskHW07;
+
+public class Task01 {
+
+	public static void main(String[] args) {
+		
+		boolean workDay=true;
+		int day=1;
+		
+		while(workDay) {
+			while(day<=6) {
+				System.out.println("I need a day off");
+				day+=1;
+				
+				
+	break;
+			}
+			
+		System.out.println("-------------------------Example Task-------");	
+		
+		
+		
+		boolean Workday=true;
+		int day1 =1;
+		
+		while (Workday) {
+			
+			if (day<6) {
+				
+				System.out.println("Ineed day off");
+				
+				
+			}else {
+				
+				System.out.println("it is weekend and I am off");
+				Workday=false;
+				
+				day1++;
+				
+				break;
+			}
+		}
+			
+		
+		
+		
+		
+		
+		
+		}
+		
+		
+		
+	}
+
+}
---
+++ src/TaskHW07/WhileLoopPractice.java
@@ -0,0 +1,28 @@
+package TaskHW07;
+
+public class WhileLoopPractice {
+
+	public static void main(String[] args) {
+		
+		
+			
+			
+			String day= "Saturday";
+			
+			if (day.equals("Saturday")); {
+				
+				System.out.println("My fevorite Java class");
+				
+				
+				
+					
+				}
+				
+				
+			
+			}
+		
+	}
+		
+
+
